0a9b63b55ea4cfe803b8a9e8c1815c9ee71f473f
Van Der Bio <33406415+W2Ning@users.noreply.github.com>
2020-02-15 16:05:15 +0800

    Update FireFoxSend_Tool.py

--- FireFoxSend_Tool.py
+++ FireFoxSend_Tool.py
@@ -2,24 +2,6 @@ import paramiko
 import os
 import sys
 
-print("""
-______ _         ______        
-|  ___(_)        |  ___|       
-| |_   _ _ __ ___| |_ _____  __
-|  _| | | '__/ _ \  _/ _ \ \/ /
-| |   | | | |  __/ || (_) >  < 
-\_|   |_|_|  \___\_| \___/_/\_\\
- _____                _        
-/  ___|              | |       
-\ `--.  ___ _ __   __| |       
- `--. \/ _ \ '_ \ / _` |       
-/\__/ /  __/ | | | (_| |       
-\____/ \___|_| |_|\__,_|       
-                               
-                               
->>>>>> press 1  git clone
->>>>>> press 2  wget
-""")
 
 selection = int(input("The way you choose: "))
 
@@ -30,7 +12,7 @@ filename = url.split("/")[-1]
 if selection == 1:
     cmd = 'git clone ' + url 
     filename_without_git = filename.split(".")[0]
-
+    filename_plus_tar = filename_without_git + ".tar"
     print("服务器正在下载")
     
 elif selection == 2:
@@ -40,19 +22,18 @@ else:
     print("wrong option!")
 
 
-filename_plus_tar = filename_without_git + ".tar"
 
 jssh = paramiko.SSHClient()
 
 jssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 
-jssh.connect('IP','22','username','password')
+jssh.connect('149.248.3.160','22','root','G{x3oJmo[%RM)GGZ')
 
 
 
 if selection == 1:
     stdin,stdout,stderr = jssh.exec_command(cmd)
-    stdin,stdout,stderr = jssh.exec_command("tar -cvf " + filename_plus_tar + "filename")
+    stdin,stdout,stderr = jssh.exec_command("tar -cvf " + filename_plus_tar + "  " +filename_without_git)
     stdin,stdout,stderr = jssh.exec_command("ffsend upload " + filename_plus_tar)
     
 elif selection == 2:
@@ -61,3 +42,6 @@ elif selection == 2:
 
 
 print(stdout.read())
+
+
+
